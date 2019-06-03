022f46b89316b51f285d604e1ef601d06283230b
estagio3 <belchior@alu.ufc.br>
2019-06-03 11:54:16 -0300

    Removidos comentarios

--- src/api/Funcionario.java
+++ src/api/Funcionario.java
@@ -11,24 +11,13 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONObject;
 
-/**
- * Servlet implementation class Funcionario
- */
 @WebServlet("/Funcionario")
 public class Funcionario extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+  
     public Funcionario() {
         super();
-        // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		 	JSONObject jsonObject = new Dados().getDados();  
 		    PrintWriter out = response.getWriter();
@@ -36,12 +25,7 @@ public class Funcionario extends HttpServlet {
 		    response.setCharacterEncoding("UTF-8");
 		    out.print(jsonObject);
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
